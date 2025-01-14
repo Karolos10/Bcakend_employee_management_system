00acafc4b098eb44306ef1d41d106b7b11832d99
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-01-14 15:42:49 -0500

    Creation of method for adding employees

--- src/main/java/com/example/employee_management_system/controller/EmployeeController.java
+++ src/main/java/com/example/employee_management_system/controller/EmployeeController.java
@@ -4,21 +4,27 @@ package com.example.employee_management_system.controller;
 import com.example.employee_management_system.model.Employee;
 import com.example.employee_management_system.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    //get all employees
     @GetMapping("/employees")
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    //create employee rest api
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
     }
 }
